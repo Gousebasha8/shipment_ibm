b7d3545131fe0b858278be3348a328413fd49791
Shaik Gouse Basha <109523597+Gousebasha8@users.noreply.github.com>
2022-08-01 09:44:28 +0530

    first commit

--- src/app/services/shipment-services.service.ts
+++ src/app/services/shipment-services.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Shipments } from '../models/shipments/shipments';
@@ -8,23 +8,13 @@ import { Shipments } from '../models/shipments/shipments';
 })
 export class ShipmentServicesService {
 
-  constructor(private _http:HttpClient) {}
-   url = 'http://localhost:3000/Shipments';
-   url2 ='http://localhost:3000/Shipment';
-   
-   
-    getShipmentResult():Observable<any>{
-      // console.log("Service...."+this.http.get()
-      return this._http.get(this.url);
+  constructor(private http:HttpClient) {}
+
+    getShipmentResult():Observable<Shipments>{
+      return this.http.get<Shipments>('http://localhost:3000/Shipments')
       
     }
-   
-      
-   getdata(){
 
-    return this._http.get(this.url);
-
-   }
   
 
 }
